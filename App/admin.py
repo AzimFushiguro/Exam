e46b08f7151379a5a99bec9c93bc1de75c9be87b
Azim <fushiguroazim@gmail.com>
2024-07-14 23:34:40 +0500

    aw

--- App/admin.py
+++ App/admin.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
-from App.models import Product,Company,Sale
+from App.models import Product, Company, Sales
 from django.contrib.auth.models import Group, User
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
-admin.site.register(Sale)
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "price", "quantity","company")
+    list_display = ("id", "title", "price", "quantity", "company")
+
+    def has_change_permission(self, request, obj=None):
+        if obj and obj.company == True:
+            return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.quantity == 0:
+            return False
+        return True
+
+
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ("id","title","product_count")
+    list_display = ("id", "title", "product_count")
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.product_count == 0:
+            return False
+        return True
+
+
+@admin.register(Sales)
+class SalesAdmin(admin.ModelAdmin):
+    list_display = ("id", "costumer_name", "product", "product_count", "sale_cost")
+
+    def has_change_permission(self, request, obj=None):
+        return False
--- App/models.py
+++ App/models.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
 
 class Company(models.Model):
     title = models.CharField("Name of company", max_length=255)
@@ -10,39 +12,45 @@ class Company(models.Model):
     class Meta:
         verbose_name = "Company"
         verbose_name_plural = "Companies"
+
+
 class Product(models.Model):  # Changed 'Products' to 'Product' to follow Django's naming conventions
+    price = models.IntegerField(default=0)
+    title = models.CharField("Maxsulot nomi", max_length=255)
+    quantity = models.IntegerField(default=0)
+    company = models.ForeignKey(Company, null=True, blank=True,
+                                on_delete=models.CASCADE)  # Updated to reference the Company model correctly
 
-        price = models.IntegerField(default=0)
-        title = models.CharField("Name of product", max_length=255)
-        quantity = models.IntegerField(default=0)
-        company = models.ForeignKey(Company, null=True, blank=True, on_delete=models.CASCADE)  # Updated to reference the Company model correctly
+    def __str__(self):
+        return self.title
 
-        def __str__(self):
-          return self.title
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
 
 
 class Sales(models.Model):
-     product = models.ForeignKey(Product.quantity, on_delete=models.CASCADE)
-     quantity = models.IntegerField()
-     sale_date = models.DateTimeField(auto_now_add=True)
+    costumer_name = models.CharField("Mijoz nomi", max_length=255)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
+    product_count = models.IntegerField("Masxulot soni")
+    sale_cost = models.IntegerField()
 
+    def __str__(self):
+        return self.costumer_name
 
-     def str(self):
-        return f"{self.quantity} units of {self.product.name} sold on {self.sale_date}"# class Product(models.Model):  # Changed 'Products' to 'Product' to follow Django's naming conventions
-#     price = models.IntegerField(default=0)
-#     title = models.CharField("Name of product", max_length=255)
-#     quantity = models.IntegerField(default=0)
-#     company = models.ForeignKey(Company, null=True, blank=True, on_delete=models.CASCADE)  # Updated to reference the Company model correctly
-#
-#     def __str__(self):
-#         return self.title
-#
-#     class Meta:
-#         verbose_name = "Product"
-#         verbose_name_plural = "Products"
-#
-# class Sale(models.Model):
-#     customer_name = models.CharField("Name of customer", max_length=255)
-#     product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
-#     product_quantity = models.ForeignKey(Company, null=True, blank=True, on_delete=models.CASCADE)
-#     sale_price = models.IntegerField(default=0)
+    class Meta:
+        verbose_name = "Sale"
+        verbose_name_plural = "Sales"
+
+    def clean(self):
+        if self.product and self.product_count > self.product.quantity:
+            raise ValidationError("Some products do not have sufficient quantity.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Perform validation before saving
+
+        if self.product:
+            self.product.quantity -= self.product_count
+            self.product.save()
+
+        super().save(*args, **kwargs)
